287a0df7c6acaf3e1cfe6894d2a6815dc0c833e0
Muhammad Ramzan <mramzan147800@gmail.com>
2026-02-06 16:48:10 +0500

    feat: implement dictionary-based word storage and set logic for unique classroom allocation

---
+++ 01_Practice/sets_dictionaries.py
@@ -0,0 +1,10 @@
+#stored some word meanings in a dictionary
+word_meanings={
+    "table":"a piece of furniture",
+    "cat":"a small pet animal",
+    "facts":"sun sets in the west." " sun rises in the east."
+}
+print(word_meanings)
+#given a list of subjects for students. Assume one classroom is required for 1 subject. How many classrooms are needed by all students 
+subjects={"Python","Java","C++","Python","Javascript","Java","Python","Java","C++","C"}
+print(len(subjects))
